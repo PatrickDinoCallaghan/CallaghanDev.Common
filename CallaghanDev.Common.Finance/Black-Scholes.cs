5fec2936c9472866373662f38c88d603c17aec96
Patrick Callaghan <patrickdcallaghan@hotmail.co.uk>
2024-10-19 08:42:22 +0100

    [Fix] - Project names to be consistant with convention used

---
+++ CallaghanDev.Common.Finance/Black-Scholes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallaghanDev.Common.Finance
+{
+    public class BlackScholes
+    {
+        /// <summary>
+        /// The Black–Scholes Formula for options pricing
+        /// </summary>
+        /// <param name="maturity"></param>
+        /// <param name="r">the annualized risk-free interest rate</param>
+        public BlackScholes(DateTime maturity, decimal r)
+        { 
+        
+        }
+    }
+}

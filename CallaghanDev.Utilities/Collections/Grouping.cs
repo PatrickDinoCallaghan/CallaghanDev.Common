df3c17b60743211c68f3edb6585bfae4c555ec52
Patrick Callaghan <patrickdcallaghan@hotmail.co.uk>
2025-04-01 12:38:48 +0100

    added grouping class to Common, and chart data

---
+++ CallaghanDev.Utilities/Collections/Grouping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallaghanDev.Utilities.Collections
+{
+    public class Grouping<TKey, TElement> : IGrouping<TKey, TElement>
+    {
+        public TKey Key { get; }
+        private readonly IEnumerable<TElement> _elements;
+
+        public Grouping(TKey key, IEnumerable<TElement> elements)
+        {
+            Key = key;
+            _elements = elements;
+        }
+
+        public IEnumerator<TElement> GetEnumerator() => _elements.GetEnumerator();
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _elements.GetEnumerator();
+        }
+    }
+}

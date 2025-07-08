49fbbb06c70ee62003cbeb17a426dcf22db883f1
Patrick Callaghan <patrickdcallaghan@hotmail.co.uk>
2025-07-08 23:48:36 +0100

    Refactoring

--- CallaghanDev.Utilities/Collections/DiskCachedList.cs
+++ CallaghanDev.Utilities/Collections/DiskCachedList.cs
@@ -11,6 +11,27 @@ namespace CallaghanDev.Utilities.Collections
 
     public static class DiskCacheExtensions
     {
+        /// <summary>
+        /// Copies all items from any IEnumerable<T> into a new DiskCachedList<T>.
+        /// </summary>
+        public static DiskCachedList<T> ToDiskCachedList<T>(
+            this IEnumerable<T> source,
+            int threshold = 10000,
+            string? storagePath = null,
+            Func<T, byte[]>? serializer = null,
+            Func<byte[], T>? deserializer = null)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            var dcl = new DiskCachedList<T>(
+                storagePath: storagePath,
+                threshold: threshold,
+                serializer: serializer,
+                deserializer: deserializer
+            );
+            dcl.AddRange(source);
+            return dcl;
+        }
         public static DiskCachedList<T> LoadFromJsonFile<T>(this DiskCachedList<T> _, string jsonFilePath, int threshold = 100_000)
         {
             return Task.Run(()=> LoadFromJsonFileAsync<T>(jsonFilePath, threshold)).Result;
@@ -150,6 +171,7 @@ namespace CallaghanDev.Utilities.Collections
             return result;
         }
     }
+
     /// <summary>
     /// A memory-efficient, disk-backed list implementation that automatically flushes items to disk
     /// when a specified in-memory threshold is exceeded. Ideal for working with large datasets that 
@@ -195,14 +217,93 @@ namespace CallaghanDev.Utilities.Collections
         private readonly Func<T, byte[]> _serializer;
         private readonly Func<byte[], T> _deserializer;
 
+        private List<(int FileNumber, long Offset)> _diskIndex = new();
+
         private static readonly HashSet<string> _usedDirectories = new();
         private static readonly object _globalLock = new();
 
         private bool _disposed = false;
 
-        private List<(int FileNumber, long Offset)> _diskIndex = new();
+        public T this[ulong index]
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (index >= _totalCount)
+                        throw new IndexOutOfRangeException();
 
-        public DiskCachedList(string? storagePath = null, int threshold = 100_000, Func<T, byte[]>? serializer = null, Func<byte[], T>? deserializer = null)
+                    // Check if it's in memory
+                    ulong diskCount = (ulong)_diskIndex.Count;
+                    if (index >= diskCount)
+                    {
+                        return _buffer[(int)(index - diskCount)];
+                    }
+
+                    var (fileNum, offset) = _diskIndex[(int)index];
+                    var file = Path.Combine(_storagePath, $"part_{fileNum:D5}.bin");
+
+                    using var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
+                    using var br = new BinaryReader(fs);
+
+                    fs.Seek(offset, SeekOrigin.Begin);
+                    int length = br.ReadInt32();
+                    var bytes = br.ReadBytes(length);
+                    return _deserializer(bytes);
+                }
+            }
+        }
+        private void RebuildIndex()
+        {
+            _diskIndex.Clear();
+            var indexFile = Path.Combine(_storagePath, $"index.bin");
+            if (File.Exists(indexFile))
+            {
+                File.Delete(indexFile);
+            }
+
+            for (int i = 0; i < _fileCounter; i++)
+            {
+                var file = Path.Combine(_storagePath, $"part_{i:D5}.bin");
+                if (!File.Exists(file)) continue;
+
+                using var fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+                using var br = new BinaryReader(fs);
+                using var indexStream = new FileStream(indexFile, FileMode.Append);
+                using var indexWriter = new BinaryWriter(indexStream);
+
+                while (fs.Position < fs.Length)
+                {
+                    long pos = fs.Position;
+                    int length = br.ReadInt32();
+                    fs.Seek(length, SeekOrigin.Current);
+
+                    indexWriter.Write(i);
+                    indexWriter.Write(pos);
+
+                    _diskIndex.Add((i, pos));
+                }
+            }
+            _totalCount = (ulong)_diskIndex.Count + (ulong)_buffer.Count;
+
+        }
+
+        private void LoadIndex()
+        {
+            var indexPath = Path.Combine(_storagePath, "index.bin");
+            if (!File.Exists(indexPath)) return;
+
+            using var fs = new FileStream(indexPath, FileMode.Open, FileAccess.Read);
+            using var br = new BinaryReader(fs);
+
+            while (fs.Position < fs.Length)
+            {
+                int fileNum = br.ReadInt32();
+                long offset = br.ReadInt64();
+                _diskIndex.Add((fileNum, offset));
+            }
+        }
+        public DiskCachedList(string? storagePath = null, int threshold = 10000, Func<T, byte[]>? serializer = null, Func<byte[], T>? deserializer = null)
         {
             _threshold = threshold;
 
@@ -258,179 +359,25 @@ namespace CallaghanDev.Utilities.Collections
                 }
             }
         }
-
-        public ulong Count => _totalCount;
-        public void FlushToDisk()
+        public void AddRange(IEnumerable<T> items)
         {
-            lock (_lock)
-            {
-                if (_buffer.Count == 0)
-                {
-                    return;
-
-                }
-
-                var fileName = Path.Combine(_storagePath, $"part_{_fileCounter:D5}.bin");
-                var indexFile = Path.Combine(_storagePath, $"index.bin");
-
-                using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
-                {
-
-                    using (var bw = new BinaryWriter(fs))
-                    {
-                        using (var indexStream = new FileStream(indexFile, FileMode.Append, FileAccess.Write, FileShare.None))
-                        {
-                            using (var indexWriter = new BinaryWriter(indexStream))
-                            {
-                                var newEntries = new List<(int, long)>();
-
-                                foreach (var item in _buffer)
-                                {
-                                    var pos = fs.Position;
-                                    var bytes = _serializer(item);
-                                    bw.Write(bytes.Length);
-                                    bw.Write(bytes);
-
-                                    indexWriter.Write(_fileCounter);
-                                    indexWriter.Write((long)pos);
-
-                                    newEntries.Add((_fileCounter, pos));
-
-                                }
-                                _diskIndex.AddRange(newEntries);
-
-                                _fileCounter++;
-                                _buffer.Clear();
-                            }
-                        }
-
-                    }
-                }
-
-            }
-        }
-
-
-        private void LoadIndex()
-        {
-            var indexPath = Path.Combine(_storagePath, "index.bin");
-            if (!File.Exists(indexPath)) return;
-
-            using var fs = new FileStream(indexPath, FileMode.Open, FileAccess.Read);
-            using var br = new BinaryReader(fs);
-
-            while (fs.Position < fs.Length)
-            {
-                int fileNum = br.ReadInt32();
-                long offset = br.ReadInt64();
-                _diskIndex.Add((fileNum, offset));
-            }
-        }
-
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-        ~DiskCachedList() => Dispose(false);
-
-        //The intent is to delete all persisted data on disposal. Free up disk space accordingly
-        private void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                lock (_globalLock)
-                {
-                    _usedDirectories.Remove(_storagePath);
-                    if (Directory.Exists(_storagePath))
-                    {
-                        Directory.Delete(_storagePath, true);
-                    }
-                }
-
-                _disposed = true;
-            }
-        }
-
-
-        public void Clear() //Shouldn't delete or removed the directory, object isn't disposed with clear, instead delete all items
-        {
-            lock (_lock)
-            {
-                _buffer.Clear();
-                _diskIndex.Clear();
-
-                _fileCounter = 0;
-                _totalCount = 0;
-
-                foreach (var file in Directory.EnumerateFiles(_storagePath, "part_*.bin"))
-                {
-                    File.Delete(file);
-                }
-            }
-        }
-        public T this[ulong index]
-        {
-            get
-            {
-                lock (_lock)
-                {
-                    if (index >= _totalCount)
-                        throw new IndexOutOfRangeException();
-
-                    // Check if it's in memory
-                    ulong diskCount = (ulong)_diskIndex.Count;
-                    if (index >= diskCount)
-                    {
-                        return _buffer[(int)(index - diskCount)];
-                    }
-
-                    var (fileNum, offset) = _diskIndex[(int)index];
-                    var file = Path.Combine(_storagePath, $"part_{fileNum:D5}.bin");
-
-                    using var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
-                    using var br = new BinaryReader(fs);
-
-                    fs.Seek(offset, SeekOrigin.Begin);
-                    int length = br.ReadInt32();
-                    var bytes = br.ReadBytes(length);
-                    return _deserializer(bytes);
-                }
-            }
-        }
-        public IEnumerator<T> GetEnumerator()
-        {
-            List<(int FileNum, long Offset)> snapshotIndex;
-            List<T> snapshotBuffer;
+            if (items == null) throw new ArgumentNullException(nameof(items));
 
             lock (_lock)
             {
-                snapshotIndex = new List<(int, long)>(_diskIndex);
-                snapshotBuffer = new List<T>(_buffer);
-            }
+                foreach (var item in items)
+                {
+                    _buffer.Add(item);
+                    _totalCount++;
 
-            foreach (var (fileNum, offset) in snapshotIndex)
-            {
-                var file = Path.Combine(_storagePath, $"part_{fileNum:D5}.bin");
-
-                using var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
-                using var br = new BinaryReader(fs);
-
-                fs.Seek(offset, SeekOrigin.Begin);
-                int length = br.ReadInt32();
-                var bytes = br.ReadBytes(length);
-                yield return _deserializer(bytes);
-            }
-
-            foreach (var item in snapshotBuffer)
-            {
-                yield return item;
+                    if (_buffer.Count >= _threshold)
+                    {
+                        FlushToDisk();
+                    }
+                }
             }
         }
 
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         public bool Remove(T item)
         {
             lock (_lock)
@@ -490,40 +437,149 @@ namespace CallaghanDev.Utilities.Collections
             }
         }
 
-        private void RebuildIndex()
+        public void FlushToDisk()
         {
-            _diskIndex.Clear();
-            var indexFile = Path.Combine(_storagePath, $"index.bin");
-            if (File.Exists(indexFile))
+            lock (_lock)
             {
-                File.Delete(indexFile);
-            }
-
-            for (int i = 0; i < _fileCounter; i++)
-            {
-                var file = Path.Combine(_storagePath, $"part_{i:D5}.bin");
-                if (!File.Exists(file)) continue;
-
-                using var fs = new FileStream(file, FileMode.Open, FileAccess.Read);
-                using var br = new BinaryReader(fs);
-                using var indexStream = new FileStream(indexFile, FileMode.Append);
-                using var indexWriter = new BinaryWriter(indexStream);
-
-                while (fs.Position < fs.Length)
+                if (_buffer.Count == 0)
                 {
-                    long pos = fs.Position;
-                    int length = br.ReadInt32();
-                    fs.Seek(length, SeekOrigin.Current);
+                    return;
 
-                    indexWriter.Write(i);
-                    indexWriter.Write(pos);
+                }
 
-                    _diskIndex.Add((i, pos));
+                var fileName = Path.Combine(_storagePath, $"part_{_fileCounter:D5}.bin");
+                var indexFile = Path.Combine(_storagePath, $"index.bin");
+
+                using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+
+                    using (var bw = new BinaryWriter(fs))
+                    {
+                        using (var indexStream = new FileStream(indexFile, FileMode.Append, FileAccess.Write, FileShare.None))
+                        {
+                            using (var indexWriter = new BinaryWriter(indexStream))
+                            {
+                                var newEntries = new List<(int, long)>();
+
+                                foreach (var item in _buffer)
+                                {
+                                    var pos = fs.Position;
+                                    var bytes = _serializer(item);
+                                    bw.Write(bytes.Length);
+                                    bw.Write(bytes);
+
+                                    indexWriter.Write(_fileCounter);
+                                    indexWriter.Write((long)pos);
+
+                                    newEntries.Add((_fileCounter, pos));
+
+                                }
+                                _diskIndex.AddRange(newEntries);
+
+                                _fileCounter++;
+                                _buffer.Clear();
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+
+        public ulong Count()
+        {
+            return _totalCount;
+        }
+
+        #region Disposal and Cleanup
+
+        ~DiskCachedList() => Dispose(false);
+        //The intent is to delete all persisted data on disposal. Free up disk space accordingly
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                lock (_globalLock)
+                {
+                    _usedDirectories.Remove(_storagePath);
+
+                    if (Directory.Exists(_storagePath))
+                    {
+                        try
+                        {
+                            Directory.Delete(_storagePath, recursive: true);
+                        }
+                        catch (IOException)
+                        {
+                            // somebody still has a file open.  Ignore and move on.
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            // no delete permission, or files are read-only.  Ignore.
+                        } 
+                    }
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        public void Clear() //Shouldn't delete or removed the directory, object isn't disposed with clear, instead delete all items
+        {
+            lock (_lock)
+            {
+                _buffer.Clear();
+                _diskIndex.Clear();
+
+                _fileCounter = 0;
+                _totalCount = 0;
+
+                foreach (var file in Directory.EnumerateFiles(_storagePath, "part_*.bin"))
+                {
+                    File.Delete(file);
                 }
             }
-            _totalCount = (ulong)_diskIndex.Count + (ulong)_buffer.Count;
-
         }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            List<(int FileNum, long Offset)> snapshotIndex;
+            List<T> snapshotBuffer;
+
+            lock (_lock)
+            {
+                snapshotIndex = new List<(int, long)>(_diskIndex);
+                snapshotBuffer = new List<T>(_buffer);
+            }
+
+            foreach (var (fileNum, offset) in snapshotIndex)
+            {
+                var file = Path.Combine(_storagePath, $"part_{fileNum:D5}.bin");
+
+                using var fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read);
+                using var br = new BinaryReader(fs);
+
+                fs.Seek(offset, SeekOrigin.Begin);
+                int length = br.ReadInt32();
+                var bytes = br.ReadBytes(length);
+                yield return _deserializer(bytes);
+            }
+
+            foreach (var item in snapshotBuffer)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         public void SaveToJsonFile( string jsonFilePath, int chunkSize = 1000) //This needs to save to file incrementally
         {

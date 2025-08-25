6bf7b43965533b6050e67645d897cbb0ec983f94
Patrick Callaghan <patrickdcallaghan@hotmail.co.uk>
2025-08-25 18:11:36 +0100

    Fix to RestAPI IG

--- CallaghanDev.Utilities/Collections/BoundedConcurrentBag.cs
+++ CallaghanDev.Utilities/Collections/BoundedConcurrentBag.cs
@@ -14,12 +14,53 @@ namespace CallaghanDev.Utilities.Collections
         public void Add(T item)
         {
             _queue.Enqueue(item);
+
             lock (_lock)
+            {
+
                 while (_queue.Count > _maxSize)
+                {
                     _queue.TryDequeue(out _);
+                }
+            }
         }
 
         public T[] ToArray() => _queue.ToArray();
+        public void AddRange(IEnumerable<T> items)
+        {
+            if (items is null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            } 
+
+            foreach (var item in items)
+            {
+                _queue.Enqueue(item);
+            }
+
+            lock (_lock)
+            {
+                int excess = _queue.Count - _maxSize;
+                for (int i = 0; i < excess; i++)
+                {
+                    _queue.TryDequeue(out _);
+                }
+            }
+        }
+        public T Last()
+        {
+            if (!Any())
+            {
+                return default(T);
+            }
+            var array = _queue.ToArray();
+            if (array.Length == 0)
+            {
+                throw new InvalidOperationException("The bag is empty.");
+            }
+            return array[^1];
+        }
+        public bool Any() => !_queue.IsEmpty;
     }
 
     public static class BoundedConcurrentQueueExtensions

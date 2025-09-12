ad4a5d26205f0c8ce4be898f9cf957559ee80221
Patrick Callaghan <patrickdcallaghan@hotmail.co.uk>
2025-09-12 09:01:12 +0100

    Lightstreamer and thread safety issues fixed as well as some API fixes

---
+++ CallaghanDev.Utilities/AtomicCounter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallaghanDev.Utilities
+{
+    public class AtomicCounter
+    {
+        private int _value;
+
+        public AtomicCounter(int initialValue = 0)
+        {
+            _value = initialValue;
+        }
+
+        /// <summary>
+        /// Returns the current value.
+        /// </summary>
+        public int Value => Volatile.Read(ref _value);
+
+        /// <summary>
+        /// Atomically increments the counter and returns the incremented value.
+        /// </summary>
+        public int Increment() => Interlocked.Increment(ref _value);
+
+        /// <summary>
+        /// Atomically decrements the counter and returns the decremented value.
+        /// </summary>
+        public int Decrement() => Interlocked.Decrement(ref _value);
+
+        /// <summary>
+        /// Adds delta to the counter and returns the new value.
+        /// </summary>
+        public int Add(int delta) => Interlocked.Add(ref _value, delta);
+
+        /// <summary>
+        /// Resets the counter to zero and returns the previous value.
+        /// </summary>
+        public int Reset() => Interlocked.Exchange(ref _value, 0);
+    }
+}
